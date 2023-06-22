c7e7f752d4238a83d3d31101fb60ea3592878d6b
Javier Renero Balgañón <jreneb00@estudiantes.unileon.es>
2023-06-22 20:16:35 +0200

    updates en el codigo

--- MS_ROS_V0.1/api/api.go
+++ MS_ROS_V0.1/api/api.go
@@ -12,92 +12,9 @@ type API struct {
 	allPages []string
 }
 
-/* type postBook struct {
-	Title string `json:"title"`
-} */
-
 type robotId struct {
 	IdRob string `json:"id"`
 }
-
-/* type BookParams struct {
-	Offset int `schema:"offset"`
-	Limit  int `schema:"limit"`
-} */
-
-/*var (
-	books   = []string{"book1", "book2", "book3", "book4", "book5"}
-	decoder = schema.NewDecoder()
-)*/
-
-/* func (a *API) getBooks(m http.ResponseWriter, r *http.Request) {
-
-	params := &BookParams{}
-
-	err := decoder.Decode(params, r.URL.Query())
-
-	if err != nil {
-		m.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if params.Offset < 0 || params.Offset > len(books) {
-		m.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if params.Limit < 0 || params.Limit > len(books) {
-		m.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	var from, to int
-
-	if params.Offset > 0 {
-		from = params.Offset
-	}
-
-	if params.Limit > 0 {
-		to = params.Limit
-	} else {
-		to = len(books)
-	}
-
-	json.NewEncoder(m).Encode(books[from:to])
-}
-
-func (a *API) getBook(m http.ResponseWriter, r *http.Request) {
-	pathParams := mux.Vars(r)
-
-	idParam := pathParams["id"]
-
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		m.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	index := id - 1
-	if index < 0 || index > len(books)-1 {
-		m.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	json.NewEncoder(m).Encode(books[index])
-}
-
-func (a *API) postBook(m http.ResponseWriter, r *http.Request) {
-	book := &postBook{}
-
-	err := json.NewDecoder(r.Body).Decode(book)
-	if err != nil {
-		m.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	books = append(books, book.Title)
-
-	m.WriteHeader(http.StatusCreated)
-} */
 
 func (a *API) handleIndex(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Message ERROR: You need to specify a valid endpoint")
@@ -162,7 +79,7 @@ func (a *API) getBattery(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if strings.Compare(batteryResp.Level, "No data") == 0 {
-		http.Error(w, "No data, So publiser Not Availeble", http.StatusNotFound)
+		http.Error(w, "No data, So publiser Not Available", http.StatusNotFound)
 		return
 	}
 
@@ -244,15 +161,15 @@ func (a *API) getOdom(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+	// error handling if the robot is not available
 	if strings.Compare(odomResp.ChildFrameID, "No data") == 0 {
-		http.Error(w, "No data, So publiser Not Availeble", http.StatusNotFound)
+		http.Error(w, "No data, So publiser Not Available", http.StatusNotFound)
 		return
 	}
 
-	print(odomResp.Position.X)
-	print(odomResp.Position.Y)
-	print(odomResp.Position.Z)
+	// print(odomResp.Position.X)
+	// print(odomResp.Position.Y)
+	// print(odomResp.Position.Z)
 
 	response := struct {
 		ID           string      `json:"id"`
--- MS_ROS_V0.1/api/routes.go
+++ MS_ROS_V0.1/api/routes.go
@@ -12,11 +12,7 @@ func (a *API) RegisterRoutes(r *mux.Router) {
 	a.allPages = append(a.allPages, "/")
 	a.allPages = append(a.allPages, "/odom")
 	a.allPages = append(a.allPages, "/discover")
-	//a.allPages = append(a.allPages, "/books")
-	//register the routes
-	/* 	r.HandleFunc("/books", a.getBooks).Methods(http.MethodGet)
-	   	r.HandleFunc("/books/{id}", a.getBook).Methods(http.MethodGet)
-	   	r.HandleFunc("/books", a.postBook).Methods(http.MethodPost) */
+
 	r.HandleFunc(a.allPages[0], a.getBattery).Methods(http.MethodGet)
 	r.HandleFunc(a.allPages[1], a.handleIndex).Methods(http.MethodGet)
 	r.HandleFunc(a.allPages[2], a.getOdom).Methods(http.MethodGet)
