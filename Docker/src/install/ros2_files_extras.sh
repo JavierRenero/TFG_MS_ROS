5f632aa9fcc1feb4e3a5ff69f45ee9883edbfa03
Javier Renero Balgañón <jreneb00@estudiantes.unileon.es>
2023-06-22 23:19:57 +0200

    cambios

--- Docker/src/install/ros2_files_extras.sh
+++ Docker/src/install/ros2_files_extras.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # clone
+
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list >/dev/null
+
 apt update
 
 apt install python3-argcomplete python3-colcon-common-extensions python3-rosdep -y 
 
 apt update
-apt install gazebo11
-apt install ros-humble-gazebo-ros-pkg ros-humble-cartographer ros-humble-cartographer-ros ros-humble-navigation2 ros-humble-nav2-bringup -y
+curl -sSL http://get.gazebosim.org | sh
+apt install ros-humble-gazebo-ros-* ros-humble-cartographer ros-humble-cartographer-ros ros-humble-navigation2 ros-humble-nav2-bringup -y
 apt install ros-humble-turtlebot3-msgs ros-humble-dynamixel-sdk ros-humble-turtlebot3 ros-humble-hls-lfcd-lds-driver -y
 
 mkdir -p /root/ros2_ws/src
@@ -27,10 +31,10 @@ git clone https://github.com/JavierRenero/ms_sub.git
 # colcon
 source /opt/ros/humble/setup.bash
 cd /root/ros2_ws
-apt install python3-pip
+apt install python3-pip -y
 pip install flask
 colcon build
-echo 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ros2_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models' >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/root/ros2_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models' >> ~/.bashrc
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
-source ~/.bashrc
+source /root/.bashrc
 source /root/ros2_ws/install/setup.sh
